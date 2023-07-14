a9a0bfaded98222a567d3ace0e70672eec28bc79
Bram Ruttens <mail@bramruttens.be>
2023-07-14 12:02:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Homey = require('homey');
+const WebSocket = require('ws');
 
 class DobissApp extends Homey.App {
 
@@ -13,6 +14,25 @@ class DobissApp extends Homey.App {
     if (!wsurl) {
       this.error('WebSocket server address is not set in the app settings');
     }
+    this.ws = new WebSocket(wsurl);
+
+    this.ws.on('open', function open() {
+      this.log('connected to CAN2WS server');
+    });
+
+    this.ws.on('close', function close() {
+      this.log('disconnected from CAN2WS server');
+    });
+
+    this.ws.on('message', function incoming(data) {
+      console.log(`received: ${data}`);
+      // Parse the incoming data
+      const message = JSON.parse(data);
+      // If the message contains the list of lights, emit an event
+      if (message.command === 'get_lights' && message.lights) {
+        this.emit('lightsList', message.lights);
+      }
+    }.bind(this));
   }
 
 }
